3115c02828bbc31add6410f9a3f61455e823f508
asifmahmud1971 <almahmudasif6@gmail.com>
2025-05-29 23:14:18 +0600

    RehavPallCareScreen

---
+++ lib/features/patient_portal/services/rehav_pall_care/view/rehav_pall_care_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RehavPallCareScreen extends StatefulWidget {
+  const RehavPallCareScreen({super.key});
+
+  @override
+  State<RehavPallCareScreen> createState() => _RehavPallCareScreenState();
+}
+
+class _RehavPallCareScreenState extends State<RehavPallCareScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
