54d3c45a5635c2e401bfc4703707688b804145f9
asifmahmud1971 <almahmudasif6@gmail.com>
2025-07-12 11:11:47 +0600

    chatting complete

--- lib/core/app/app_dependency.config.dart
+++ lib/core/app/app_dependency.config.dart
@@ -8,31 +8,102 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:demo/core/app/app_dependency.dart' as _i18;
-import 'package:demo/core/app/app_preference.dart' as _i3;
-import 'package:demo/data/network/api_client.dart' as _i14;
-import 'package:demo/data/network/api_request.dart' as _i11;
-import 'package:demo/features/screens/auth/sign_in/cubit/sign_in_cubit.dart'
-    as _i7;
-import 'package:demo/features/screens/auth/sign_in/repository/sign_in_repository.dart'
-    as _i12;
-import 'package:demo/features/screens/auth/sign_in/repository/sign_in_repository_imp.dart'
-    as _i13;
-import 'package:demo/features/screens/auth/sign_up/cubit/sign_up_cubit.dart'
-    as _i8;
-import 'package:demo/features/screens/auth/sign_up/repository/sign_up_repository.dart'
-    as _i16;
-import 'package:demo/features/screens/auth/sign_up/repository/sign_up_repository_imp.dart'
-    as _i17;
-import 'package:demo/features/screens/home/cubit/home_cubit.dart' as _i6;
-import 'package:demo/features/screens/home/repository/home_repo_imp.dart'
-    as _i15;
-import 'package:demo/features/screens/no_internet/cubit/internet_cubit.dart'
-    as _i10;
-import 'package:demo/features/screens/theme/cubit/theme_cubit.dart' as _i9;
 import 'package:dio/dio.dart' as _i5;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
+import 'package:medPilot/core/app/app_dependency.dart' as _i55;
+import 'package:medPilot/core/app/app_preference.dart' as _i3;
+import 'package:medPilot/data/network/api_client.dart' as _i26;
+import 'package:medPilot/data/network/api_request.dart' as _i10;
+import 'package:medPilot/features/auth/forgot_password/cubit/forgot_password_cubit.dart'
+    as _i51;
+import 'package:medPilot/features/auth/forgot_password/repository/forgot_password_repository.dart'
+    as _i50;
+import 'package:medPilot/features/auth/forgot_password/repository/forgot_password_repository_imp.dart'
+    as _i49;
+import 'package:medPilot/features/auth/sign_in/cubit/sign_in_cubit.dart'
+    as _i53;
+import 'package:medPilot/features/auth/sign_in/repository/sign_in_repository.dart'
+    as _i42;
+import 'package:medPilot/features/auth/sign_in/repository/sign_in_repository_imp.dart'
+    as _i41;
+import 'package:medPilot/features/auth/sign_up/cubit/sign_up_cubit.dart' as _i6;
+import 'package:medPilot/features/auth/sign_up/repository/sign_up_repository.dart'
+    as _i32;
+import 'package:medPilot/features/auth/sign_up/repository/sign_up_repository_imp.dart'
+    as _i33;
+import 'package:medPilot/features/dashboard/cubit/dashboard_cubit.dart' as _i7;
+import 'package:medPilot/features/no_internet/cubit/internet_cubit.dart' as _i9;
+import 'package:medPilot/features/patient_portal/chat/cubit/chat_cubit.dart'
+    as _i39;
+import 'package:medPilot/features/patient_portal/chat/repository/chat_repository.dart'
+    as _i15;
+import 'package:medPilot/features/patient_portal/chat/repository/chat_repository_imp.dart'
+    as _i16;
+import 'package:medPilot/features/patient_portal/home/cubit/home_cubit.dart'
+    as _i38;
+import 'package:medPilot/features/patient_portal/home/repository/home_repository.dart'
+    as _i19;
+import 'package:medPilot/features/patient_portal/home/repository/home_repository_imp.dart'
+    as _i20;
+import 'package:medPilot/features/patient_portal/on_demand_service/cubit/onDemand_service_cubit.dart'
+    as _i47;
+import 'package:medPilot/features/patient_portal/on_demand_service/repository/onDemandService_repository.dart'
+    as _i17;
+import 'package:medPilot/features/patient_portal/on_demand_service/repository/onDemandService_repository_imp.dart'
+    as _i18;
+import 'package:medPilot/features/patient_portal/services/cubit/services_cubit.dart'
+    as _i22;
+import 'package:medPilot/features/patient_portal/services/follow_up/cubit/followup_cubit.dart'
+    as _i21;
+import 'package:medPilot/features/patient_portal/services/invesigation/cubit/investigation_cubit.dart'
+    as _i54;
+import 'package:medPilot/features/patient_portal/services/invesigation/repository/investigation_repository.dart'
+    as _i46;
+import 'package:medPilot/features/patient_portal/services/invesigation/repository/investigation_repository_imp.dart'
+    as _i45;
+import 'package:medPilot/features/patient_portal/services/pain_clinic/cubit/painClinic_cubit.dart'
+    as _i24;
+import 'package:medPilot/features/patient_portal/services/psych_social_clinic/cubit/psych_social_cubit.dart'
+    as _i52;
+import 'package:medPilot/features/patient_portal/services/psych_social_clinic/repository/psych_social_repository.dart'
+    as _i44;
+import 'package:medPilot/features/patient_portal/services/psych_social_clinic/repository/psych_social_repository_imp.dart'
+    as _i43;
+import 'package:medPilot/features/patient_portal/services/rehav_pall_care/cubit/rehab_pall_cubit.dart'
+    as _i40;
+import 'package:medPilot/features/patient_portal/services/rehav_pall_care/repository/rehab_pall_repository.dart'
+    as _i35;
+import 'package:medPilot/features/patient_portal/services/rehav_pall_care/repository/rehab_pall_repository_imp.dart'
+    as _i34;
+import 'package:medPilot/features/patient_portal/services/repository/service_repository.dart'
+    as _i11;
+import 'package:medPilot/features/patient_portal/services/repository/service_repository_imp.dart'
+    as _i12;
+import 'package:medPilot/features/patient_portal/services/upload_document/cubit/document_cubit.dart'
+    as _i23;
+import 'package:medPilot/features/patient_portal/services/wound_clinic/cubit/woundClinic_cubit.dart'
+    as _i25;
+import 'package:medPilot/features/profile/cubit/profile_cubit.dart' as _i48;
+import 'package:medPilot/features/profile/repository/profile_repository.dart'
+    as _i37;
+import 'package:medPilot/features/profile/repository/profile_repository_imp.dart'
+    as _i36;
+import 'package:medPilot/features/staff_portal/attendence/cubit/attendance_cubit.dart'
+    as _i31;
+import 'package:medPilot/features/staff_portal/medicine_alert/cubit/medicine_alert_cubit.dart'
+    as _i27;
+import 'package:medPilot/features/staff_portal/pescription/cubit/staff_prescription_cubit.dart'
+    as _i30;
+import 'package:medPilot/features/staff_portal/repository/staffPortal_repository.dart'
+    as _i13;
+import 'package:medPilot/features/staff_portal/repository/staffPortal_repository_imp.dart'
+    as _i14;
+import 'package:medPilot/features/staff_portal/roaster/cubit/roster_cubit.dart'
+    as _i28;
+import 'package:medPilot/features/staff_portal/task/cubit/task_cubit.dart'
+    as _i29;
+import 'package:medPilot/features/theme/cubit/theme_cubit.dart' as _i8;
 import 'package:shared_preferences/shared_preferences.dart' as _i4;
 
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -53,20 +124,73 @@ Future<_i1.GetIt> $initGetIt(
     preResolve: true,
   );
   gh.factory<_i5.Dio>(() => appModule.dio);
-  gh.factory<_i6.HomeCubit>(() => _i6.HomeCubit());
-  gh.factory<_i7.SignInCubit>(() => _i7.SignInCubit());
-  gh.factory<_i8.SignUpCubit>(() => _i8.SignUpCubit());
-  gh.factory<_i9.ThemeCubit>(() => _i9.ThemeCubit());
-  gh.factory<_i10.InternetCubit>(() => _i10.InternetCubit());
-  gh.factory<_i11.ApiRequest>(() => _i11.ApiRequest());
-  gh.factory<_i12.SignInRepository>(
-      () => _i13.SignInRepositoryImp(apiRequest: gh<_i11.ApiRequest>()));
-  gh.factory<_i14.ApiClient>(() => _i14.ApiClient(gh<_i5.Dio>()));
-  gh.factory<_i15.HomeRepositoryImp>(
-      () => _i15.HomeRepositoryImp(apiRequest: gh<_i11.ApiRequest>()));
-  gh.factory<_i16.SignUpRepository>(
-      () => _i17.SignUpRepositoryImp(apiRequest: gh<_i11.ApiRequest>()));
+  gh.factory<_i6.SignUpCubit>(() => _i6.SignUpCubit());
+  gh.factory<_i7.DashboardCubit>(() => _i7.DashboardCubit());
+  gh.factory<_i8.ThemeCubit>(() => _i8.ThemeCubit());
+  gh.factory<_i9.InternetCubit>(() => _i9.InternetCubit());
+  gh.factory<_i10.ApiRequest>(() => _i10.ApiRequest());
+  gh.factory<_i11.ServiceRepository>(
+      () => _i12.ServiceRepositoryImp(apiRequest: gh<_i10.ApiRequest>()));
+  gh.factory<_i13.StaffPortalRepository>(
+      () => _i14.StaffPortalRepositoryImp(apiRequest: gh<_i10.ApiRequest>()));
+  gh.factory<_i15.ChatRepository>(
+      () => _i16.ChatRepositoryImp(apiRequest: gh<_i10.ApiRequest>()));
+  gh.factory<_i17.OnDemandServiceRepository>(() =>
+      _i18.OnDemandServiceRepositoryImp(apiRequest: gh<_i10.ApiRequest>()));
+  gh.factory<_i19.HomeRepository>(
+      () => _i20.HomeRepositoryImp(apiRequest: gh<_i10.ApiRequest>()));
+  gh.factory<_i21.FollowUpCubit>(
+      () => _i21.FollowUpCubit(gh<_i11.ServiceRepository>()));
+  gh.factory<_i22.ServiceCubit>(
+      () => _i22.ServiceCubit(gh<_i11.ServiceRepository>()));
+  gh.factory<_i23.DocumentCubit>(
+      () => _i23.DocumentCubit(gh<_i11.ServiceRepository>()));
+  gh.factory<_i24.PainClinicCubit>(
+      () => _i24.PainClinicCubit(gh<_i11.ServiceRepository>()));
+  gh.factory<_i25.WoundClinicCubit>(
+      () => _i25.WoundClinicCubit(gh<_i11.ServiceRepository>()));
+  gh.factory<_i26.ApiClient>(() => _i26.ApiClient(gh<_i5.Dio>()));
+  gh.factory<_i27.MedicineAlertCubit>(
+      () => _i27.MedicineAlertCubit(gh<_i13.StaffPortalRepository>()));
+  gh.factory<_i28.RosterCubit>(
+      () => _i28.RosterCubit(gh<_i13.StaffPortalRepository>()));
+  gh.factory<_i29.TaskCubit>(
+      () => _i29.TaskCubit(gh<_i13.StaffPortalRepository>()));
+  gh.factory<_i30.StaffPrescriptionCubit>(
+      () => _i30.StaffPrescriptionCubit(gh<_i13.StaffPortalRepository>()));
+  gh.factory<_i31.AttendanceCubit>(
+      () => _i31.AttendanceCubit(gh<_i13.StaffPortalRepository>()));
+  gh.factory<_i32.SignUpRepository>(
+      () => _i33.SignUpRepositoryImp(apiRequest: gh<_i10.ApiRequest>()));
+  gh.factory<_i34.RehabPallRepositoryImp>(
+      () => _i35.RehabPallRepository(apiRequest: gh<_i10.ApiRequest>()));
+  gh.factory<_i36.ProfileRepositoryImp>(
+      () => _i37.ProfileRepository(apiClient: gh<_i26.ApiClient>()));
+  gh.factory<_i38.HomeCubit>(() => _i38.HomeCubit(gh<_i19.HomeRepository>()));
+  gh.factory<_i39.ChatCubit>(() => _i39.ChatCubit(gh<_i15.ChatRepository>()));
+  gh.factory<_i40.RehabPallCubit>(
+      () => _i40.RehabPallCubit(gh<_i34.RehabPallRepositoryImp>()));
+  gh.factory<_i41.SignInRepositoryImp>(
+      () => _i42.SignInRepository(apiClient: gh<_i26.ApiClient>()));
+  gh.factory<_i43.PsychSocialRepositoryImp>(
+      () => _i44.PsychSocialRepository(apiClient: gh<_i26.ApiClient>()));
+  gh.factory<_i45.InvestigationRepositoryImp>(
+      () => _i46.InvestigationRepository(apiClient: gh<_i26.ApiClient>()));
+  gh.factory<_i47.OnDemandServiceCubit>(
+      () => _i47.OnDemandServiceCubit(gh<_i17.OnDemandServiceRepository>()));
+  gh.factory<_i48.ProfileCubit>(
+      () => _i48.ProfileCubit(gh<_i36.ProfileRepositoryImp>()));
+  gh.factory<_i49.ForgotPasswordRepositoryImp>(
+      () => _i50.ForgotPasswordRepository(apiClient: gh<_i26.ApiClient>()));
+  gh.factory<_i51.ForgotPasswordCubit>(
+      () => _i51.ForgotPasswordCubit(gh<_i49.ForgotPasswordRepositoryImp>()));
+  gh.factory<_i52.PsychSocialCubit>(
+      () => _i52.PsychSocialCubit(gh<_i43.PsychSocialRepositoryImp>()));
+  gh.factory<_i53.SignInCubit>(
+      () => _i53.SignInCubit(gh<_i41.SignInRepositoryImp>()));
+  gh.factory<_i54.InvestigationCubit>(
+      () => _i54.InvestigationCubit(gh<_i45.InvestigationRepositoryImp>()));
   return getIt;
 }
 
-class _$AppModule extends _i18.AppModule {}
+class _$AppModule extends _i55.AppModule {}
--- lib/features/patient_portal/chat/view/chat_screen.dart
+++ lib/features/patient_portal/chat/view/chat_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:intl/intl.dart';
 import 'package:medPilot/core/constants/app_colors.dart';
 import 'package:medPilot/core/enum/app_status.dart';
 import 'package:medPilot/features/patient_portal/chat/cubit/chat_cubit.dart';
 import 'package:medPilot/features/patient_portal/chat/model/chat_model.dart';
-
-
 
 class PalliativeChatScreen extends StatefulWidget {
   const PalliativeChatScreen({super.key});
@@ -15,59 +14,56 @@ class PalliativeChatScreen extends StatefulWidget {
   State<PalliativeChatScreen> createState() => _PalliativeChatScreenState();
 }
 
-
-
 enum MessageStatus { sent, delivered, read }
 
 class _PalliativeChatScreenState extends State<PalliativeChatScreen> {
   final TextEditingController _messageController = TextEditingController();
 
-
   void _sendMessage() {
     if (_messageController.text.trim().isEmpty) return;
 
     context.read<ChatCubit>().getChat(_messageController.text);
-      _messageController.clear();
-    
+    _messageController.clear();
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ChatCubit, ChatState>(
-  builder: (context, state) {
-    return Scaffold(
-      appBar: _buildAppBar(),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFFF8F9FD),
-              Color(0xFFFCF4ED), // Light peach to complement orange
-            ],
+      builder: (context, state) {
+        return Scaffold(
+          appBar: _buildAppBar(),
+          body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFFF8F9FD),
+                  Color(0xFFFCF4ED), // Light peach to complement orange
+                ],
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(child: _buildMessageList()),
+                Visibility(
+                    visible: state.appStatus == AppStatus.loading,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: _buildMessageBubble(Message(
+                          text: "Processing..",
+                          time: DateTime.now(),
+                          isSentByMe: false)),
+                    )),
+                _buildMessageInput(),
+              ],
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(child: _buildMessageList()),
-            Visibility(
-                visible: state.appStatus == AppStatus.loading,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: _buildMessageBubble(Message(text: "Processing..", time: DateTime.now(), isSentByMe: false)),
-                )),
-            _buildMessageInput(),
-          ],
-        ),
-      ),
+        );
+      },
     );
-  },
-);
   }
-
-
 
   PreferredSizeWidget _buildAppBar() {
     return AppBar(
@@ -127,17 +123,21 @@ class _PalliativeChatScreenState extends State<PalliativeChatScreen> {
 
   Widget _buildMessageList() {
     return BlocBuilder<ChatCubit, ChatState>(
-  builder: (context, state) {
-    return (state.messages??[]).isNotEmpty? ListView.builder(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-      reverse: true,
-      itemCount: state.messages?.length,
-      itemBuilder: (context, index) {
-        return _buildMessageBubble(state.messages?[index]??Message());
+      builder: (context, state) {
+        return (state.messages ?? []).isNotEmpty
+            ? ListView.builder(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                reverse: true,
+                itemCount: state.messages?.length,
+                itemBuilder: (context, index) {
+                  return _buildMessageBubble(
+                      state.messages?[index] ?? Message());
+                },
+              )
+            : SizedBox();
       },
-    ):SizedBox();
-  },
-);
+    );
   }
 
   Widget _buildMessageBubble(Message message) {
@@ -145,54 +145,75 @@ class _PalliativeChatScreenState extends State<PalliativeChatScreen> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 20),
       child: Column(
-        crossAxisAlignment: isMe??false ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        crossAxisAlignment:
+            isMe ?? false ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
           Container(
-            constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * 0.75,
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            decoration: BoxDecoration(
-              color: isMe??false ? AppColors.kPrimaryColor : const Color(0xFFFCF4ED),
-              borderRadius: BorderRadius.only(
-                topLeft: const Radius.circular(20),
-                topRight: const Radius.circular(20),
-                bottomLeft: Radius.circular(isMe??false ? 20 : 0),
-                bottomRight: Radius.circular(isMe??false ? 0 : 20),
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.75,
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.05),
-                  blurRadius: 8,
-                  offset: const Offset(0, 4),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(
+                color: isMe ?? false
+                    ? AppColors.kPrimaryColor
+                    : const Color(0xFFFCF4ED),
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(20),
+                  topRight: const Radius.circular(20),
+                  bottomLeft: Radius.circular(isMe ?? false ? 20 : 0),
+                  bottomRight: Radius.circular(isMe ?? false ? 0 : 20),
                 ),
-              ],
-            ),
-            child: Text(
-              message.text??"",
-              style: TextStyle(
-                color: isMe??false ? Colors.white : const Color(0xFF5D4037),
-                fontSize: 15,
-                height: 1.4,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 8,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
               ),
-            ),
-          ),
+              child: message.isSentByMe ?? false
+                  ? Text(
+                      message.text ?? "",
+                      style: TextStyle(
+                        color: isMe ?? false
+                            ? Colors.white
+                            : const Color(0xFF5D4037),
+                        fontSize: 15,
+                        height: 1.4,
+                      ),
+                    )
+                  : Html(
+                      data: message.text ?? "",
+                      style: {
+                        // Global styles can be applied here
+                        "body": Style(
+                          margin: Margins.zero,
+                          padding: HtmlPaddings.zero,
+                        ),
+                        "p": Style(
+                          fontSize: FontSize(16.0),
+                          lineHeight: LineHeight(1.5),
+                        ),
+                      },
+                    )),
           const SizedBox(height: 6),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
             child: Row(
               mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: isMe??false ? MainAxisAlignment.end : MainAxisAlignment.start,
+              mainAxisAlignment: isMe ?? false
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
               children: [
                 Text(
-                  DateFormat('h:mm a').format(message.time??DateTime.now()),
+                  DateFormat('h:mm a').format(message.time ?? DateTime.now()),
                   style: TextStyle(
                     color: AppColors.kPrimaryColor.withOpacity(0.7),
                     fontSize: 11,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
-                if (isMe??false) ...[
+                if (isMe ?? false) ...[
                   const SizedBox(width: 6),
                   Icon(
                     message.status == MessageStatus.read
@@ -228,7 +249,7 @@ class _PalliativeChatScreenState extends State<PalliativeChatScreen> {
       child: SafeArea(
         child: Row(
           children: [
-           /* IconButton(
+            /* IconButton(
               icon: Icon(Icons.photo, color: AppColors.kPrimaryColor, size: 28),
               onPressed: () {},
             ),*/
@@ -247,14 +268,16 @@ class _PalliativeChatScreenState extends State<PalliativeChatScreen> {
                         decoration: InputDecoration(
                           hintText: "Type a message...",
                           border: InputBorder.none,
-                          hintStyle: TextStyle(color: AppColors.kPrimaryColor.withOpacity(0.5)),
+                          hintStyle: TextStyle(
+                              color: AppColors.kPrimaryColor.withOpacity(0.5)),
                         ),
                         minLines: 1,
                         maxLines: 3,
                       ),
                     ),
                     IconButton(
-                      icon: Icon(Icons.emoji_emotions_outlined, color: AppColors.kPrimaryColor),
+                      icon: Icon(Icons.emoji_emotions_outlined,
+                          color: AppColors.kPrimaryColor),
                       onPressed: () {},
                     ),
                   ],
